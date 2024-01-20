be4c6c2795ccafc409df6ee1eeaf3732a54627fe
Arib Dhuka <atxarib99@gmail.com>
2024-01-20 13:13:00 -0600

    Fixed logic, was inverted

--- engine/filters/connectivity.py
+++ engine/filters/connectivity.py
@@ -5,8 +5,9 @@ class Connectivity(Filter):
 
     def __init__(self, connectivity: str):
         self.valid_input(connectivity)
+        self.connectivity = connectivity
         
-        Filter.__init__(filter_val=connectivity)
+        Filter.__init__(self, filter_val=connectivity)
 
 
     def check(self, board):
@@ -21,11 +22,11 @@ class Connectivity(Filter):
             return self.distance(board, 3)
 
     def distance(self, board, distance):
-        if abs(board[0].evaluate() - board[1].evaluate()) < distance:
+        if abs(board[0].evaluate() - board[1].evaluate()) > distance:
             return False
-        if abs(board[1].evaluate() - board[2].evaluate()) < distance:
+        if abs(board[1].evaluate() - board[2].evaluate()) > distance:
             return False
-        if abs(board[0].evaluate() - board[2].evaluate()) < distance:
+        if abs(board[0].evaluate() - board[2].evaluate()) > distance:
             return False
 
         return True
