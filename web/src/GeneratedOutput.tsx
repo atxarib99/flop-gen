fa2fbb01cd1cb5531abcbd9d814c5ec5ebb6a2bc
Arib Dhuka <atxarib99@gmail.com>
2024-07-03 00:33:33 -0500

    Added copy functionality for generated output

--- web/src/GeneratedOutput.tsx
+++ web/src/GeneratedOutput.tsx
@@ -1,13 +1,29 @@
 import React, {Component, useEffect} from "react";
 import * as deck from '@letele/playing-cards';
-import List from '@mui/material/List';
+import {List, Button, Snackbar} from '@mui/material'; 
+import Slide, { SlideProps } from '@mui/material/Slide';
 
 type GeneratedProps = {
 	generated: String[];
 	hand?: ["1d", "1c", "1s", "1h"];
 };
 
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction="up" />;
+}
+
 function GeneratedOutput(props: GeneratedProps) {
+
+  const [open, setOpen] = React.useState(false);
+	const [snackbarMessage, setMessage] = React.useState("");
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
 	function stringToCards(str: String): string[] {
 
@@ -21,35 +37,83 @@ function GeneratedOutput(props: GeneratedProps) {
 
 		return Cards;
 	}
+	
+	function useCopyClick() {
+		console.log('copy');
+		copyToClipboard(props.generated.toString());
+	};
+
+	const copyToClipboard = (str) => {
+		const el = document.createElement("textarea");
+		el.value = str;
+		el.setAttribute("readonly", "");
+		el.style.position = "absolute";
+		el.style.left = "-9999px";
+		document.body.appendChild(el);
+		try {
+			const selected =
+				document.getSelection().rangeCount > 0
+					? document.getSelection().getRangeAt(0)
+					: false;
+			el.select();
+			document.execCommand("copy");
+			document.body.removeChild(el);
+			if (selected) {
+				document.getSelection().removeAllRanges();
+				document.getSelection().addRange(selected);
+			}
+			setMessage("Copied!");
+			setOpen(true);
+
+		} catch(e: unknown) {
+			setMessage("Couldn't copy!");
+			setOpen(true);
+		}
+	};
+	
 
 	return (
-		<List
-      sx={{
-        width: '100%',
-        bgcolor: '#EEEEEE',
-        position: 'relative',
-        overflow: 'auto',
-				height: '100vh',
-        '& ul': { padding: 0 },
-      }}
-      subheader={<li />}
-    >	
-			{
-				props.generated.map((val, index) => {
-					const Cards = stringToCards(val);
-					console.log(Cards);
-					return (
-						<div>
-							{Cards.map((val2, index) => {
-								const Card = deck[val2];
-								return (
-									<Card style={{ height: '10%', width: '10%' }} />	
-								)
-							})}
-						</div>
-					)})
-			}
-		</List>
+		<div className="fullWidth">
+			<Button variant="contained" onClick={useCopyClick}>
+				Copy
+			</Button>
+			<List
+				sx={{
+					width: '100%',
+					bgcolor: '#EEEEEE',
+					position: 'relative',
+					overflow: 'auto',
+					height: '100vh',
+					'& ul': { padding: 0 },
+				}}
+				subheader={<li />}
+			>	
+				{
+					props.generated.map((val, index) => {
+						const Cards = stringToCards(val);
+						console.log(Cards);
+						return (
+							<div>
+								{Cards.map((val2, index) => {
+									const Card = deck[val2];
+									return (
+										<Card style={{ height: '10%', width: '10%' }} />	
+									)
+								})}
+							</div>
+						)})
+				}
+			</List>
+
+			<Snackbar
+      anchorOrigin={{ vertical:'bottom', horizontal:'center'}}
+			open={open}
+			autoHideDuration={2000}
+			onClose={handleClose}
+			message={snackbarMessage}
+			TransitionComponent={Slide}
+			/>	
+		</div>
 	);
 
 }
